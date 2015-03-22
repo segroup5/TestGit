589fc6116ea5bf63483dc6c5a232a3181bc7e49d
Felix Liu <felix_liu@outlook.com>
2015-03-22 22:19:24 +0800

    Initial commitment

---
+++ src/TestGit.java
@@ -0,0 +1,19 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		printLYM();
+		printFJT();
+	}
+
+	private static void printFJT() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void printLYM() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
